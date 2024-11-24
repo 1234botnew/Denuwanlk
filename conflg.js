514bad6478c0d749ea669058eeca6fc6bafa1a1d
1234botnew <denuwanbot244@gmail.com>
2024-11-24 12:27:12 +0530

    Update conflg.js

--- conflg.js
+++ conflg.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process. env.ALIVE_IMG || "https://drive.google.com/file/d/1-zm5zbhk3E8wKVivPszpYrel_B6vtYFm/view?usp=drivesdk",
+ALIVE_MSG: process. env.ALIVE_MSG || "Hello i am alive owner Denuwan-md",   
 };
